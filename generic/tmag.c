3da470ebdab2033b5c3dc8c157e35378dc0238d2
Matthias Kraft <M.Kraft@gmx.com>
2008-12-06 16:58:01 +0000

    refactored, cleaned up, commented sources; fixed merge; bumped version, missing: documentation, tests, performance (1500 µs/iteration is too much);

--- generic/tmag.c
+++ generic/tmag.c
@@ -1,4 +1,4 @@
-/*
+/* --------------------------------------------------------------------------
  * tmag.c --
  *
  *	This file implements a Tcl interface to the libmagic functions.
@@ -8,53 +8,71 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * [FIXME] refactor and make configurable, consider file(1) as reference
- */
+ * -------------------------------------------------------------------------- */
 
+/* libmagic header */
 #include <magic.h>
-
+/* Tcl header */
 #include <tcl.h>
-
+/* tmag header */
 #include "tmag.h"
 
-/* open and init lib magic
- */
+/* --------------------------------------------------------------------------
+ * TmagSetErrorResult --
+ *
+ * Create Tcl objects from strings and set them as interp result.
+ * -------------------------------------------------------------------------- */
+
+void TmagSetErrorResult(Tcl_Interp *interp, const char *message, const char *reason) {
+  Tcl_Obj *result_obj;
+
+  result_obj = Tcl_NewStringObj(message, -1);
+  Tcl_AppendToObj(result_obj, reason, -1);
+  Tcl_SetObjResult(interp, result_obj);
+}
+
+/* --------------------------------------------------------------------------
+ * TmagInit --
+ *
+ * Initialize a new libmagic session and load the default database.
+ * -------------------------------------------------------------------------- */
 
 magic_t TmagInit(Tcl_Interp *interp, int flags) {
   magic_t magic_h;
-  Tcl_Obj *result_obj;
 
   if ((magic_h = magic_open(MAGIC_ERROR|MAGIC_RAW|flags)) == NULL) {
-    result_obj = Tcl_NewStringObj("inititializing libmagic failed: ", -1);
-    Tcl_AppendToObj(result_obj, magic_error(magic_h), -1);
-    Tcl_SetObjResult(interp, result_obj);
-    return NULL;
-  }
+    TmagSetErrorResult(interp, TMAG_OPEN_ERROR_MSG, magic_error(magic_h));
 
-  /* would use whatever compiled in default database is, or if MAGIC environment
-   * variable is set, uses this */
-  if ((magic_load(magic_h, NULL)) != 0) {
-    result_obj = Tcl_NewStringObj("loading the default libmagic database failed: ", -1);
-    Tcl_AppendToObj(result_obj, magic_error(magic_h), -1);
-    Tcl_SetObjResult(interp, result_obj);
+  } else if ((magic_load(magic_h, NULL)) != 0) {
+    TmagSetErrorResult(interp, TMAG_LOAD_ERROR_MSG, magic_error(magic_h));
     magic_close(magic_h);
-    return NULL;
+    magic_h = NULL;
   }
 
   return magic_h;
 }
 
-/* libmagic::filetype filename ?(-isbuffer|-follow)? ?-all?
- * 
- */
-int TmagTypeCmd(ClientData data, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[]) {
-    magic_t magic_h;
-    const char *result_str, *fname_str, *flags[] = {"-isbuffer", "-follow", "-all", NULL};
-    const unsigned char *buffer_ptr;
-    enum flagIdx {IsBufferIdx, FollowIdx, AllIdx};
-    Tcl_Obj *result_obj;
-    int buffer_len, rc=TCL_OK, flag_isbuffer=0, flag_follow=0, flag_all=0, magic_flags=0;
+/* --------------------------------------------------------------------------
+ * TmagFileCmd --
+ *
+ * Implements Tcl command
+ * libmagic::filetype filename ?(-isbuffer|-follow)? ?-all?
+ *
+ * Returns result string from libmagic or an error.
+ * -------------------------------------------------------------------------- */
 
+int TmagFileCmd(ClientData data, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[]) {
+    magic_t magic_h;
+    const char *result_str, *fname_str=NULL;
+    const unsigned char *buffer_ptr=NULL;
+    int buffer_len=0, rc=TCL_OK, flag_isbuffer=0, flag_follow=0, magic_flags=0;
+
+    /* possible flags */
+    const char *flags[] = {"-isbuffer", "-follow", "-all", NULL};
+    enum flagsIdx {IsBufferIdx, FollowIdx, AllIdx};
+
+    /* check possible range of arguments and process them,
+     * note that -isbuffer and -follow are mutual exclusive */
     if (objc < 2 || objc > 4) {
 	Tcl_WrongNumArgs(interp, 1, objv, "filename ?flags ...?");
 	return TCL_ERROR;
@@ -73,57 +91,66 @@ int TmagTypeCmd(ClientData data, Tcl_Interp *interp, int objc, Tcl_Obj *CONST ob
 	magic_flags |= MAGIC_SYMLINK;
 	break;
       case AllIdx:
-	flag_all = 1;
 	magic_flags |= MAGIC_CONTINUE;
 	break;
       }
       objc--;
     }
     if (flag_follow && flag_isbuffer) {
-      Tcl_SetObjResult(interp, Tcl_NewStringObj( "cannot resolve symlinks on buffer", -1 ));
+      TmagSetErrorResult(interp, TMAG_ILL_FLAGS_MSG, TMAG_ILL_FLAGS_RSN);
       return TCL_ERROR;
     }
-
     if (flag_isbuffer) {
       buffer_ptr = Tcl_GetByteArrayFromObj(objv[1], &buffer_len);
     } else {
       fname_str = Tcl_GetString(objv[1]);
     }
 
+    /* now that we have all required input, init libmagic */
     if ((magic_h = TmagInit(interp, magic_flags)) == NULL) {
         return TCL_ERROR;
     }
 
+    /* identify content */
     if (flag_isbuffer) {
       result_str = magic_buffer(magic_h, (const void *)buffer_ptr, buffer_len);
     } else {
       result_str = magic_file(magic_h, fname_str);
     }
 
+    /* check for error, setup result, close libmagic session */
     if (result_str == NULL) {
-      char * errmsg = (flag_isbuffer ? "buffer type identification failed: " : "file type identification failed: ");
-      result_obj = Tcl_NewStringObj(errmsg, -1);
-      Tcl_AppendToObj(result_obj, magic_error(magic_h), -1);
+      TmagSetErrorResult(interp, (flag_isbuffer ? TMAG_BUFFER_ERROR_MSG : TMAG_FILE_ERROR_MSG), magic_error(magic_h));
       rc = TCL_ERROR;
     } else {
-      result_obj = Tcl_NewStringObj(result_str, -1);
+      Tcl_SetObjResult(interp, Tcl_NewStringObj(result_str, -1));
     }
     magic_close(magic_h);
-    Tcl_SetObjResult(interp, result_obj);
     return rc;
 }
 
-/* libmagic::mimetype filename ?(-isbuffer|-follow)? ?-all? ?-with-encoding?
- * see also documentation of fileutil::magic::mimetype
- */
+/* --------------------------------------------------------------------------
+ * TmagMimeCmd --
+ *
+ * Implements Tcl command
+ * libmagic::mimetype filename ?(-isbuffer|-follow)? ?-all? ?-with-encoding?
+ *
+ * Returns the mime type as result string, with a string identifying the
+ * encoding optionally appended. May return an error.
+ * -------------------------------------------------------------------------- */
+
 int TmagMimeCmd(ClientData data, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[]) {
     magic_t magic_h;
-    const char *result_str, *fname_str, *flags[] = {"-isbuffer", "-follow", "-all", "-with-encoding", NULL};
-    const unsigned char *buffer_ptr;
-    enum flagIdx {IsBufferIdx, FollowIdx, AllIdx, WithEncodingIdx};
-    Tcl_Obj *result_obj;
-    int buffer_len, rc=TCL_OK, flag_isbuffer=0, flag_follow=0, flag_all=0, flag_with_encoding=0, magic_flags=MAGIC_MIME_TYPE;
+    const char *result_str, *fname_str=NULL;
+    const unsigned char *buffer_ptr=NULL;
+    int buffer_len=0, rc=TCL_OK, flag_isbuffer=0, flag_follow=0, magic_flags=MAGIC_MIME_TYPE;
 
+    /* possible flags */
+    const char *flags[] = {"-isbuffer", "-follow", "-all", "-with-encoding", NULL};
+    enum flagsIdx {IsBufferIdx, FollowIdx, AllIdx, WithEncodingIdx};
+
+    /* check possible range of arguments and process them,
+     * note that -isbuffer and -follow are mutual exclusive */
     if (objc < 2 || objc > 5) {
 	Tcl_WrongNumArgs(interp, 1, objv, "filename ?flags ...?");
 	return TCL_ERROR;
@@ -142,54 +169,53 @@ int TmagMimeCmd(ClientData data, Tcl_Interp *interp, int objc, Tcl_Obj *CONST ob
 	magic_flags |= MAGIC_SYMLINK;
 	break;
       case AllIdx:
-	flag_all = 1;
 	magic_flags |= MAGIC_CONTINUE;
 	break;
       case WithEncodingIdx:
-	flag_with_encoding = 1;
 	magic_flags |= MAGIC_MIME_ENCODING;
 	break;
       }
       objc--;
     }
     if (flag_follow && flag_isbuffer) {
-      Tcl_SetObjResult(interp, Tcl_NewStringObj( "cannot resolve symlinks on buffer", -1 ));
+      TmagSetErrorResult(interp, TMAG_ILL_FLAGS_MSG, TMAG_ILL_FLAGS_RSN);
       return TCL_ERROR;
     }
-
     if (flag_isbuffer) {
       buffer_ptr = Tcl_GetByteArrayFromObj(objv[1], &buffer_len);
     } else {
       fname_str = Tcl_GetString(objv[1]);
     }
 
+    /* now that we have all required input, init libmagic */
     if ((magic_h = TmagInit(interp, magic_flags)) == NULL) {
         return TCL_ERROR;
     }
 
+    /* identify content */
     if (flag_isbuffer) {
       result_str = magic_buffer(magic_h, (const void *)buffer_ptr, buffer_len);
     } else {
       result_str = magic_file(magic_h, fname_str);
     }
 
+    /* check for error, setup result, close libmagic session */
     if (result_str == NULL) {
-      char * errmsg = (flag_isbuffer ? "buffer type identification failed: " : "file type identification failed: ");
-      result_obj = Tcl_NewStringObj(errmsg, -1);
-      Tcl_AppendToObj(result_obj, magic_error(magic_h), -1);
+      TmagSetErrorResult(interp, (flag_isbuffer ? TMAG_BUFFER_ERROR_MSG : TMAG_FILE_ERROR_MSG), magic_error(magic_h));
       rc = TCL_ERROR;
     } else {
-      result_obj = Tcl_NewStringObj(result_str, -1);
+      Tcl_SetObjResult(interp, Tcl_NewStringObj(result_str, -1));
     }
     magic_close(magic_h);
-    Tcl_SetObjResult(interp, result_obj);
     return rc;
 }
 
-/*
+/* --------------------------------------------------------------------------
  * Tmag_Init --
- * Register namespace and commands with the tcl interpreter.
- */
+ *
+ * Register namespace, commands, and package with the Tcl interpreter.
+ * -------------------------------------------------------------------------- */
+
 int Tmag_Init(Tcl_Interp *interp) {
     Tcl_Namespace *nsPtr; /* pointer to hold our own new namespace */
 
@@ -202,7 +228,8 @@ int Tmag_Init(Tcl_Interp *interp) {
         return TCL_ERROR;
     }
 
-    Tcl_CreateObjCommand(interp, TMAG_NS "::type", TmagFileCmd, NULL, NULL);
+    Tcl_CreateObjCommand(interp, TMAG_NS "::filetype", TmagFileCmd, NULL, NULL);
+    Tcl_CreateObjCommand(interp, TMAG_NS "::mimetype", TmagMimeCmd, NULL, NULL);
 
     if (Tcl_PkgProvide(interp, TMAG_EXT_NAME, TMAG_EXT_VERSION) == TCL_ERROR) {
         return TCL_ERROR;
@@ -210,3 +237,4 @@ int Tmag_Init(Tcl_Interp *interp) {
 
     return TCL_OK;
 }
+
